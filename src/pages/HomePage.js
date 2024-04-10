4e7bd3fd10a4e97a3061f45bc6977e35f27d5a9d
ebbak <ebka2201@student.miun.se>
2024-04-10 13:28:29 +0200

    Strukturerat upp filerna och gjort ett skal i html & css

---
+++ src/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function HomePage(){
+    return (
+        <div className="App">
+
+
+            <aside className="welcomeheadline">
+                <h1>
+                    Välkommen till Sollefteå alpina
+                </h1>
+            </aside>
+
+            <aside className="rightsidebar">
+                <h2>
+                    Bli medlem
+                </h2>
+            </aside>
+
+
+        </div>
+    )
+}
+
+export default HomePage;
---
+++ src/pages/TrainingPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function TrainingPage(){
+    return (
+        <div className="App">
+
+
+            <aside className="welcomeheadline">
+                <h1>
+                    Träning
+                </h1>
+            </aside>
+
+
+
+
+        </div>
+    )
+}
+
+export default TrainingPage;
