b390d447cf2c815dc47b0de0c3bdf7474313f1cd
ebbak <ebka2201@student.miun.se>
2024-04-17 13:57:06 +0200

    Första utkast för html och css på kontakta oss, bara så att jag kunde se så backend funkar.

--- src/pages/ContactUsPage.js
+++ src/pages/ContactUsPage.js
@@ -19,7 +19,7 @@ function ContactUsPage(){
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:8080/api/mail', {
+            const response = await fetch('http://localhost:8080/api/email', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
