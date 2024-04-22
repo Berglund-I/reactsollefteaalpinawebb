510d1bcd41e75a8b34b1cc61e9dc9763a055366f
Berglund-I <idbe2204@student.miun.se>
2024-04-22 08:54:53 +0200

    Facebook plugin works and are on the homepage and trainingpage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,7 +9,15 @@ function HomePage(){
                 <h1>
                     Välkommen till Sollefteå alpina
                 </h1>
-                <NewsComponent />
+                <div className="fb-page"
+                     data-href="https://www.facebook.com/p/Sollefte%C3%A5-Alpina-Klubb-100063470472728/"
+                     data-tabs="timeline" data-width="600" data-height="500" data-small-header="false"
+                     data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true">
+                    <blockquote cite="https://www.facebook.com/profile.php?id=100063470472728"
+                                className="fb-xfbml-parse-ignore"><a
+                        href="https://www.facebook.com/profile.php?id=100063470472728">Sollefteå Alpina Klubb</a>
+                    </blockquote>
+                </div>
 
             </aside>
 
