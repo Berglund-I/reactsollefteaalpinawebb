5139203efc63c8565b967aeea200fe1f19b412df
Berglund-I <idbe2204@student.miun.se>
2024-04-29 10:43:22 +0200

    Link icon from bootstrap in CompetitionPage.js and CompetitionCalendarComponent.js. And "npm install bootstrap-icons" is added in the terminal.

--- src/pages/CompetitionPage.js
+++ src/pages/CompetitionPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CompetitionCalendarComponent from "../components/CompetitionCalendarComponent";
 
+
 function CompetitionPage(){
 
     return (
@@ -9,14 +10,16 @@ function CompetitionPage(){
 
                 <article className="competition-home">
                     <h2 className="competition-home-title">Hemmatävlingar</h2>
-                    <p className="competition-home-text">Här kan du se tävlingar som vi i Sollefteå alpina klubb är med och arrangera</p>
-                    <CompetitionCalendarComponent />
+                    <p className="competition-home-text">Här kan du se tävlingar som vi i Sollefteå alpina klubb är med
+                        och arrangera</p>
+                    <CompetitionCalendarComponent/>
                 </article>
 
                 <article className="competition-calender">
                     <h2 className="competition-calender-title">Tävlingskalendern</h2>
                     <p className="competition-calender-text">Här kan du se och anmäla dig till kommande tävlingar</p>
-                    <a href="https://ta.skidor.com/EventCalendar.aspx?orgid=80" className="button-to-competitionCalendar">Klicka här för att gå till Tävlingskalendern</a>
+                    <a href="https://ta.skidor.com/EventCalendar.aspx?orgid=80"
+                       className="button-to-competitionCalendar">Klicka här för att gå till Tävlingskalendern <i className="bi bi-box-arrow-up-right"></i> </a>
                 </article>
 
             </section>
