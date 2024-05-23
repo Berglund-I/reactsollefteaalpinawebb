1edd81b217a6ddef0a81fdb02b2f5487e77cbd02
ebbak <ebka2201@student.miun.se>
2024-05-23 15:25:39 +0200

    Nu funkar det med bilderna när vi publicerat sidan

--- src/components/SidebarComponent.js
+++ src/components/SidebarComponent.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Link from '@mui/material/Link';
 import Typography from "@mui/material/Typography";
 
-function SidebarComponent(){
+function SidebarComponent() {
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/facebook-pictures')
+            .then(response => response.json())
+            .then(data => {
+                setPhotos(data);
+            });
+    }, []);
+
     return (
         <Grid container direction="column" spacing={2}>
 
@@ -22,9 +32,21 @@ function SidebarComponent(){
                         <i className="bi bi-instagram">Sollefteaalpina</i>
                     </Link>
                     <Link variant="body1" href="https://www.facebook.com/profile.php?id=100063470472728&sk=photos" target="_blank"
-                       rel="noopener noreferrer">
+                          rel="noopener noreferrer">
                         <i className="bi bi-facebook">Sollefteå alpina klubb</i>
                     </Link>
+                </Paper>
+            </Grid>
+
+            <Grid item>
+                <Paper>
+                    <Typography variant="h2">Våra senaste bilder</Typography>
+                    {photos.map(photo => {
+                        const url = `https://www.facebook.com/photo.php?fbid=${photo.id}/&set=pb.100063470472728.-2207520000&type=3`;
+                        return (
+                            <img key={photo.id} src={url} alt="Facebook photo"/>
+                        );
+                    })}
                 </Paper>
             </Grid>
 
