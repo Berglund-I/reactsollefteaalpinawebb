17b9a028ea5969d8f934fe364447c8e05069e977
Berglund-I <idbe2204@student.miun.se>
2024-05-27 11:55:48 +0200

    Done

--- src/components/HeaderElements.js
+++ src/components/HeaderElements.js
@@ -7,7 +7,7 @@ function HeaderElements(){
     return (
         <Box className="App-header" display="flex" alignItems="center" sx={{ justifyContent: { xs: 'center', sm: 'flex-start' } }}>
             <Link to={"/Hem"}>
-                <img src={process.env.PUBLIC_URL + 'logosollefteaalpinaklubb.png'} alt="Sollefteå alpinas logga och länk till hem" style={{ width: '250px', height: 'auto' }}/>
+                <img src={process.env.PUBLIC_URL + 'logosollefteaalpinaklubb.webp'} alt="Sollefteå alpinas logga och länk till hem" style={{ width: '250px', height: 'auto' }}/>
             </Link>
             <Typography variant="h1" sx={{ display: { xs: 'none', sm: 'block' } }}>
                 SOLLEFTEÅ ALPINA SKIDKLUBB
